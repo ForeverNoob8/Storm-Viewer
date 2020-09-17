13a7c80dd3943c8cb2cebc22e5e3aced050d9077
Toni <toni.asikainen@icloud.com>
2020-09-17 19:51:09 +0300

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Toni on 17.9.2020.
+//  Copyright © 2020 asikainen.toni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
